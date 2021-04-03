36cf2df8fcd2bc33c179209ca03d40f2a882bdf8
Frederick Thomas <supercodingninja@outlook.com>
2021-04-03 03:44:28 -0700

    Re-starting Approach: page does not render on local as before.

--- src/components/Preloader.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../index.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-function Preloader(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
-}
-
-export default Preloader;
