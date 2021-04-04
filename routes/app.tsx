8687f676a9e3768e1b3653c1d46ca3908a502fa4
Frederick Thomas <supercodingninja@outlook.com>
2021-04-04 00:04:08 -0700

    Web API Integration

--- routes/app.tsx
+++ routes/app.tsx
@@ -50,4 +50,29 @@ app.post('/api/email', (req, res, next) => {
         });
 });
 
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+// Javascript //
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const msg = {
+  to: 'supercodingninja@outlook.com', // Change to your recipient
+  from: 'supercodingninja@outlook.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent')
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
+
 app.listen(3030, '0.0.0.0');
